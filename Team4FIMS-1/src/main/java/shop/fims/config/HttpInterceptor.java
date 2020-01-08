0fff2eebcc1a456bdb1da011afd9ce5201cd51db
KORDONGSEOK <54506544+KORDONGSEOK@users.noreply.github.com>
2020-01-08 17:19:12 +0900

    [김동석] 권한별 로그인 설정 완료 - 2020-01-08

---
+++ Team4FIMS-1/src/main/java/shop/fims/config/HttpInterceptor.java
@@ -0,0 +1,40 @@
+package shop.fims.config;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+@Component
+public class HttpInterceptor extends HandlerInterceptorAdapter {
+
+	private static final Logger logger = LoggerFactory.getLogger(HttpInterceptor.class);
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request,
+							 HttpServletResponse response,
+							 Object handler) {
+		logger.info("================ Before Method");
+		return true;
+	}
+	
+	@Override
+	public void postHandle( HttpServletRequest request,
+							HttpServletResponse response,
+							Object handler,
+							ModelAndView modelAndView) {
+		logger.info("================ Method Executed");
+	}
+	
+	@Override
+	public void afterCompletion(HttpServletRequest request,
+								HttpServletResponse response, 
+								Object handler, 
+								Exception ex) {
+		logger.info("================ Method Completed");
+	}
+}
